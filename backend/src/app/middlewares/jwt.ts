89c62b72aac28a5acb04d1f7d5d640b0faff080e
Jose Breiner <jbpaig@gmail.com>
2021-02-15 19:17:16 -0500

    verificacion de token

--- backend/src/app/middlewares/jwt.ts
+++ backend/src/app/middlewares/jwt.ts
@@ -1,8 +1,10 @@
-import { sign,verify } from 'jsonwebtoken';
-import { log } from '@config/logger'
+import { sign,verify,VerifyErrors } from 'jsonwebtoken';
+import { log } from '@config/logger';
 import { Request,Response,NextFunction } from 'express';
+import i_jwt from '@interfaces/jwt';
 
-export const generarToken = ( uid:string,username:string ) => {
+
+export const generarToken = ( {uid,username}:i_jwt ) => {
     
     const { SECRET_JWT_SEED } = process.env;
     const payload = { uid,  username }
@@ -11,3 +13,37 @@ export const generarToken = ( uid:string,username:string ) => {
         expiresIn: '2h'
     })
 }
+
+
+export const validarToken = ( req:Request, res:Response,next:NextFunction ) => {
+    try {
+        
+        const token = req.header('x-token');
+        const { SECRET_JWT_SEED } = process.env;
+
+        if( !token ) return res.status(403).json({
+            ok: false, 
+            msg: 'No existe el token'
+        })
+
+        verify( token,String(SECRET_JWT_SEED),(err,payload) => {
+            if( err ) {
+                log.error(err)
+                return res.status( 400 ).json({
+                    ok: false,
+                    msg: 'Por favor contacte a un administrador'
+                })
+            }
+
+            req.body.tokenPayload = payload;
+            return next();
+        })
+
+    } catch (error) {
+        log.error(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor contacte a un administrador'
+        })
+    }
+}   
